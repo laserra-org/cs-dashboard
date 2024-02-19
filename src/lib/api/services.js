4476f5afa7b3306a5fde63a0a2a4097f64e87447
Paolo Innocenti <paolo.innocenti@laserradesca.com>
2024-02-19 16:13:10 +0100

    cors hapio error

--- src/lib/api/services.js
+++ src/lib/api/services.js
@@ -7,11 +7,10 @@ export async function getServiceList(header, params='') {
     const queryString = Object.keys(params).length > 0
     ? `?${new URLSearchParams(params).toString()}`
     : '';
-    const response = await fetch(HAPIO_API_URL + '' + queryString, {
+    const response = await fetch(HAPIO_API_URL + '/v1/services/' + queryString, {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${HAPIO_API_TOKEN}`, // Include the JWT token in the header
-        'Content-Type': 'application/json',
       },
     });
     return response;
